c0411a5de500d563f623517d7db1ef971acafbbc
babbu <>
2021-05-23 23:30:04 +0000

    dps fiz o contador de letras modificadas( sim infelizmente ficou " a palavra é : numero de letras mudadas e dps a palavra nova" mas nem tudo na vida são flores)

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@ string replace(string s, char c1, char c2)
     for (int i = 0; i < l; i++) {
       
         if (s[i] == c1){
-            s[i] = c2; ;
+            s[i] = c2;
+            j++;
         }
     }
+    cout << "houve " << j << " letra(s) mudada(s)" << endl;
     return s;
 }
 
