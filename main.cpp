908e5e5f0a9b85b91fe312d50f49d27be5195fcc
babbu <>
2021-05-23 23:24:19 +0000

    comecei colocando o namespace std, declarando as variáveis locais e o "básico" do código

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+using namespace std;
+
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  string s;
+  char c1;
+  char c2;
+
+  cout << "digite a palavra: " << endl;
+  cin >> s;
+
+  cout << "digite a letra original para ser trocada: " << endl;
+  cin >> c1;
+
+  cout << " digite a letra substituta: " << endl;
+  cin >> c2;
+
+
+  cout << "a palavra era: " << s << endl;
+  
+return 0;
 }
