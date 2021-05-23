21f81038bcb70ff9cfb8273512570b56709b68c6
babbu <>
2021-05-23 23:34:21 +0000

    então fiz os ponteiros das palavras nova e velha respectivamente ( e sim ainda com o problema do "save" passado)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 string replace(string s, char c1, char c2)
 {
+  string *ponteiro;
+  ponteiro = &s;
+  string n;
+  string *ponteiron;
+  ponteiron = &n;
     int l = s.length();
     int j = 0;
     for (int i = 0; i < l; i++) {
@@ -10,9 +15,11 @@ string replace(string s, char c1, char c2)
         if (s[i] == c1){
             s[i] = c2;
             j++;
+            n = s;
         }
     }
     cout << "houve " << j << " letra(s) mudada(s)" << endl;
+    cout << "endereço da palavra nova: " << &n << endl;
     return s;
 }
 
@@ -33,5 +40,6 @@ int main() {
 
   cout << "a palavra era: " << s << endl;
   cout << "a palavra é: " << replace(s, c1, c2) << endl;
+  cout << "endereço da palavra anterior: " << &s;
 return 0;
 }
